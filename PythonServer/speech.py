818dfe3fa84e5a4918fe01c9a9cfb2495c26474f
Rahulbiradar9 <rahulbiradar7134@gmail.com>
2026-03-18 11:23:57 +0530

    Clean repo for new project

--- PythonServer/speech.py
+++
@@ -1,7 +0,0 @@
-from faster_whisper import WhisperModel
-
-model = WhisperModel("base", device="cpu", compute_type="int8")
-
-def speech_to_text(audio_path):
-    segments, _ = model.transcribe(audio_path)
-    return " ".join(segment.text.strip() for segment in segments)
